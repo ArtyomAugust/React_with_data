40a1d9b69839ccd5455eb01630bc2927fcc9ca17
Artyom Ustinov <artyomustinov@gmail.com>
2023-12-04 01:03:26 +0200

    progress goes

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,121 +1,160 @@
 import "./Table.css";
 import { Link } from 'react-router-dom';
-import {Button, Container, Row, Col, Pagination, InputGroup, Form }from 'react-bootstrap';
-import Tables from 'react-bootstrap/Table';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import useSWR, { mutate } from 'swr';
+import dayjs, { Dayjs } from 'dayjs';
+import * as isBetween from 'dayjs/plugin/isBetween';
+import { useState, useCallback, useEffect } from "react";
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DateField } from '@mui/x-date-pickers/DateField';
 
-const Table = () => {
 
-    const makeTable = () => {
-        return (
-            <>
-            </>
-        );
-    };
+
+const  Table = () => {
+
+    const fetcher =(url: string )=> fetch(url).then(r => r.json())
+    const { data, error, isLoading, mutate } = useSWR("https://opendata.ecdc.europa.eu/covid19/casedistribution/json/", fetcher);
+    mutate(data);
+    const [vStart, setStart] = useState<Dayjs | null>(dayjs('2019-12-31'));
+    const [vEnd, setEnd] = useState<Dayjs | null>(dayjs('2020-12-14'));
+
+    const minDate = dayjs("2019-12-31");
+    const maxDate = dayjs('2020-12-14') ;
+    let rows: GridRowsProp = [];
+    console.log(dayjs(vStart).format('DD/MM/YYYY'), vEnd, "timeS");
+    console.log(vEnd, "timeE")
+
+      
+    const columns: GridColDef[] = [
+        { field: 'countriesAndTerritories', headerName: 'Страна', width: 150 },
+        { field: 'cases', headerName: 'Количество случаев', width: 250 },
+        { field: 'deaths', headerName: 'Количество смертей', width: 250 },
+        { field: 'casesAll', headerName: 'Количество случаев всего', width: 250 },
+        { field: 'deathsAll', headerName: 'Количество смертей всего', width: 250 },
+        { field: 'Cumulative_number_for_14_days_of_COVID-19_cases_per_100000', 
+        headerName: 'Количество случаев на 100000 жителей', width: 300 },
+        { field: 'quantityDeath1000', headerName: 'Количество смертей на 1000 жителей', width: 300 }
+    ];
+
+
+    useCallback(() => {
+        setTimeout(()=>{
+            makeTable(data);
+        },1000)
+        console.log("useCallback");
+        
+    }, [vStart, vEnd]);
+
+    const  makeTable = (dataSer: any) => {
+
+        let countCases: number = 0;
+        let countDeaths: number = 0;
+        let cntryAll = []; 
+        let record: any = [];
+        let arrayData: any = [];
+        // let arrayColumns: any = [];
+
+
+        if (dataSer !== undefined) {
+            record = dataSer.records;
+            console.log(record, "record");
+            //Adding id
+            record.forEach((item:any, i: any): any => {
+                item.id = i + 1;
+            });
+
+            const dateReg = record.filter((item:any, i: any): any => {
+                const [day, month, year] = item.dateRep.split('/');
+                return dayjs(`${year}-${month}-${day}`).isBetween('2020-01-31', '2020-11-14', 'day', '[]');
+            });
+            console.log(dateReg, "dateReg");
+            cntryAll = record.map((item: any) => {
+                return item.countriesAndTerritories;
+            });
+            
+            cntryAll = cntryAll.filter((value: any, index: any, self: any) => {
+                return self.indexOf(value) === index;
+            });
+            // console.log(cntryAll,'cntryAll');
+            // const [day, month, year] = '23/05/2020'.split('/');
+            // const datee = dayjs(`${year}-${month}-${day}`).isBetween('2019-12-31', '2020-12-14', 'day', '[]');
+            
+            
+            for (const iterator of cntryAll) {
+                const array = record.filter( (elem: any) => {  
+                    return elem.countriesAndTerritories === iterator;  
+                  });
+                // console.log(array, "array");
+                
+                array.reverse();
+                arrayData.push(...array);
+            }
+            console.log(arrayData,'arrayData');
+            
+            
+            // console.log(cntryAll, "cntryAll");
+            for (const iterator of cntryAll) {
+                
+                arrayData.forEach((item: any, i: any, array: any) => 
+                {
+                    if (item.countriesAndTerritories === iterator) {
+                        countCases += array[i].cases;
+                        item.casesAll = countCases;
+                        countDeaths += array[i].deaths;
+                        item.deathsAll = countDeaths;
+                    } 
+                });
+                countDeaths = 0;
+                countCases = 0;
+            }
+            rows = [...arrayData];
+            
+        }
+
+    }
+
+    makeTable(data);
 
     return(  
-        <>
-        <div >
-            <Container style={{background: "#74bbce", border: "2px solid"}}>
-                <Row>
-                    <Col><button>Фильтровать по полю...</button>
-                        <Form.Select size="sm">
-                                <option>Small select</option>
-                        </Form.Select>
-                    </Col>
-                    <InputGroup className="mb-3" size="sm" style={{width: "15%"}}>
-                        <Form.Control
-                        placeholder="Поиск страны..."
-                        aria-label="Recipient's username"
-                        aria-describedby="basic-addon2"
-                        />
-                        <Button variant="outline-secondary" id="button-addon2">
-                        Button
-                        </Button>
-                        <Form.Control
-                        placeholder="Значение от"
-                        aria-label="Recipient's username"
-                        aria-describedby="basic-addon2"
-                        />
-                        <Form.Control
-                        placeholder="Значение до"
-                        aria-label="Recipient's username"
-                        aria-describedby="basic-addon2"
-                        />
-                    </InputGroup>
-                        <button>Значение от</button>
-                        <button>Значение до</button>
+        <>  
+            <div>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                    label="Controlled picker"
+                    value={vStart}
+                    format="DD-MM-YYYY"
+                    defaultValue={dayjs('2019-12-31')}//yyyy-mm-dd
+                    minDate={minDate}
+                    maxDate={maxDate}
+                    onChange={(newValue, context) =>{ 
+                        if (context.validationError == null) {
+                            setStart(newValue);
+                        }
+                    }}
+                    /> 
+                    <DatePicker
+                    label="Controlled picker"
+                    value={vEnd}
+                    format="DD-MM-YYYY"
+                    defaultValue={dayjs('2020-12-14')}
+                    minDate={minDate}
+                    maxDate={maxDate}
+                    onChange={(newValue, context) =>{ 
+                        if (context.validationError == null) {
+                            setEnd(newValue);
+                        }
+                    }}
+                    />
+                </LocalizationProvider>
+            </div>
+            <div style={{ height: 500, width: '100%'}}>
+                <DataGrid rows={rows} columns={columns} />
+            </div>
 
-                </Row>
-                <Row className="mb-3">
-                    <Col lg={{ offset: 10}}>
-                        <button>Сбросить фильтры</button>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Tables striped bordered hover>
-                            <thead className="table-warning">
-                                <tr>
-                                <th>Страна</th>
-                                <th>Количество случаев</th>
-                                <th>Количество смертей</th>
-                                <th>Количество случаев всего</th>
-                                <th>Количество смертей всего</th>
-                                <th>Количество случаев на 1000 жителей</th>
-                                <th>Количество сметей на 1000 жителей</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr className="table-light">
-                                <td>1</td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                <td>a</td>
-                                <td>s</td>
-                                <td>f</td>
-                                </tr>
-                                <tr className="table-secondary">
-                                <td>2</td>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                                <td>s</td>
-                                <td>d</td>
-                                <td>v</td>
-                                </tr>
-                                <tr>
-                                <td>3</td>
-                                <td>Larry the Bird</td>
-                                <td>@twitter</td>
-                                <td>3</td>
-                                <td>Larry the Bird</td>
-                                <td>@twitter</td>
-                                <td>@twitter</td>
-                                </tr>
-                            </tbody>
-                        </Tables>
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col lg={{ offset: 9}}>
-                        <Pagination>
-                            <Pagination.First  />
-                            <Pagination.Item>{1}</Pagination.Item>
-                            <Pagination.Item>{2}</Pagination.Item>
-                            <Pagination.Item>{3}</Pagination.Item>
-                            <Pagination.Item>{4}</Pagination.Item>
-                            <Pagination.Ellipsis />
-                            <Pagination.Last />
-                        </Pagination>
-                    </Col>
-                </Row>
-
-            </Container>
-        </div>
         </>
       
     );
